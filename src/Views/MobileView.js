96a52e0e3cf3d33133fb8ffc41e7b0e590273758
Diego Grassino <dg27ar@MacBook-Pro-de-Diego.local>
2022-01-27 02:08:32 -0300

    Make the mobile layout

--- src/Views/MobileView.js
+++ src/Views/MobileView.js
@@ -1,19 +1,43 @@
-import { Box, Flex, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Text, Heading } from '@chakra-ui/react';
 
 function MobileView() {
   return (
     <Box
       minHeight="100vh"
+      paddingTop="45px"
+      paddingX="45px"
+      paddingBottom="60px"
       bgImage="url('assets/mobile-background.png')"
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
     >
-      <Box>
+      <Box minHeight="50vh">
         <Flex>
-          <Box>Title</Box>
+          <Box color="red.600">
+            <Heading as="h2" size="lg">
+              Nominis
+            </Heading>
+          </Box>
           <Spacer />
           <Box>Hearts</Box>
-          <Box>User</Box>
+          <Spacer />
+          <Box alignContent="end">Volume</Box>
+        </Flex>
+        <Box>
+          <Text marginTop="30px" fontSize="4xl" fontWeight="500">
+            Let&apos;s practice
+          </Text>
+          <Text marginTop="15px" fontSize="xl">
+            Put the words in order to make a correct sentence
+          </Text>
+        </Box>
+      </Box>
+      <Box>
+        <Flex>
+          <Spacer />
+          <Box marginStart="20%" minWidth="35%">
+            Game
+          </Box>
         </Flex>
       </Box>
     </Box>
